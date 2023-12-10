9b6897380016f0280e38f1afe3fb277d219c0baa
DmitryTurlai <turlay08@gmail.com>
2023-12-10 22:50:34 +0300

    C:\Users\Acer\PycharmProjects\pythonProject2>git clone https://github.com/DmitryTurlai/Stepik_HomeWorks.git

--- main.py
+++ main.py
@@ -1,36 +1,17 @@
 s = input().split()
 n = int(s[0])
 m = int(s[1])
-k = 1
 mas = [[0] * m for _ in range(n)]
-i0 = j0 = 0
-im = jm = 1
+nm = 0
 mas[0][0] = 1
 
+for q in range(n * m):
+    for i in range(n):
+        for j in range(m):
+            if i+j == q:
+                nm += 1
+                mas[i][j] = nm
 
-def diag(mas, k, id, jd):
-    for j in range(jd + 1):
-        mas[j][jd - j] = k
-        k += 1
-    return mas, k
-
-def diag(mas, k, id, jd):
-    for j in range(jd + 1):
-        mas[j][jd - j] = k
-        k += 1
-    return mas, k
-def diag2(mas, k, id, jd):
-    for j in range(jd + 1):
-        mas[j - id][jd - j] = k
-        k += 1
-    return mas, k
-
-
-for q in range(m):
-    mas, k = diag(mas, k, 0, q)
-
-for q in range(1, n):
-    mas, k = diag2(mas, k, q, m)
 
 for i in range(n):
     for j in range(m):
